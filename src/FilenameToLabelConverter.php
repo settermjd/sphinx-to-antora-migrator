e399212e8bd09bbcdf3c93e743edead9eac5d690
Matthew Setter <matthew@matthewsetter.com>
2018-03-22 11:18:07 +0100

    First commit to the project
    This adds the core source and test files, Composer configuration files,
    and supplementary files (testing and code style) to the repository.

---
+++ src/FilenameToLabelConverter.php
@@ -0,0 +1,116 @@
+<?php
+
+namespace FilenameToLabelConverter;
+
+/**
+ * Class FilenameToLabelConverter
+ * @package FilenameToLabelConverter
+ */
+class FilenameToLabelConverter
+{
+    const SMALL_WORDS = [
+        'of','a','the','and','an','or','nor','but','is','if',
+        'then','else','when', 'at','from','by','on','off',
+        'for','in','out','over','to','into','with'
+    ];
+
+    const CORE_REPLACEMENTS = ['_', '-'];
+
+    /**
+     * @var array A list of words that must always be upper-cased
+     */
+    const ALWAYS_UPPERCASE = [
+        'api',
+        'css',
+        'db',
+        'ftp',
+        'gui',
+        'imap',
+        'js',
+        'ldap',
+        'nginx',
+        'oc',
+        'occ',
+        'ocs',
+        'php',
+        'sftp',
+        'smb',
+        'ssl',
+        'ui',
+        'url',
+        'l10n',
+    ];
+
+    /**
+     * A list of custom words that have to be rendered case-sensitive
+     *
+     * The key/value representation is just a simple way to make the
+     * data structure easier to work with.
+     */
+    const CUSTOM_WORDS = [
+        'amazons3' => 'AmazonS3',
+        'antivirus' => 'AntiVirus',
+        'clamav' => 'ClamAV',
+        'javascript' => 'JavaScript',
+        'onedrive' => 'OneDrive',
+        'openstack' => 'OpenStack',
+        'owncloud' => 'ownCloud',
+        'selinux' => 'SELinux',
+    ];
+
+    /**
+     * @param string $filename
+     * @return string
+     */
+    public function convert(string $filename) : string
+    {
+        return $this->fixTitlecase(
+            $this->convertCustomWords(
+                $this->convertUppercaseWords(
+                    str_replace(self::CORE_REPLACEMENTS, ' ', $filename)
+                )
+            )
+        );
+    }
+
+    private function fixTitlecase(string $text) : string
+    {
+        return preg_replace_callback(
+            sprintf('/\b(%s)\b/i', implode('|', self::SMALL_WORDS)),
+            function ($match) {
+                return strtolower($match[0]);
+            },
+            ucwords($text)
+        );
+    }
+
+    /**
+     * @param string $filename
+     * @return string
+     */
+    private function convertUppercaseWords(string $filename) : string
+    {
+        return preg_replace_callback(
+            sprintf("/(%s)/i", implode('|', self::ALWAYS_UPPERCASE)),
+            function ($match) {
+                return strtoupper($match[0]);
+            },
+            $filename
+        );
+    }
+
+    /**
+     * @param string $filename
+     * @return string
+     */
+    private function convertCustomWords(string $filename) : string
+    {
+        return preg_replace_callback(
+            sprintf("/(%s)/i", implode('|', array_keys(self::CUSTOM_WORDS))),
+            function ($match) {
+                return self::CUSTOM_WORDS[strtolower($match[0])];
+            },
+            $filename
+        );
+    }
+}
---
+++ test/FilenameToLabelConverterTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace FilenameToLabelConverter\Tests;
+
+use FilenameToLabelConverter\FilenameToLabelConverter;
+use PHPUnit\Framework\TestCase;
+
+class FilenameToLabelConverterTest extends TestCase
+{
+    public function converterDataProvider()
+    {
+        return [
+            ['javascript_and_css', 'JavaScript and CSS'],
+            ['activity_configuration', 'Activity Configuration'],
+            ['ldap_proxy_cache_server_setup', 'LDAP Proxy Cache Server Setup'],
+            ['index_php_less_urls', 'Index PHP Less URLs'],
+            ['background_jobs_configuration', 'Background Jobs Configuration'],
+            ['import_ssl_cert', 'Import SSL Cert'],
+            ['user_provisioning_api', 'User Provisioning API'],
+            ['antivirus_configuration', 'AntiVirus Configuration'],
+            ['Ocs-recipient-api', 'OCS Recipient API'],
+            ['two-factor-provider', 'Two Factor Provider'],
+            ['l10n', 'L10N'],
+            ['nginx_configuration', 'NGINX Configuration'],
+            ['selinux_configuration', 'SELinux Configuration'],
+            ['user_auth_ftp_smb_imap', 'User Auth FTP SMB IMAP'],
+            ['openstack', 'OpenStack'],
+            ['onedrive', 'OneDrive'],
+            ['clamav', 'ClamAV'],
+        ];
+    }
+
+    /**
+     * @dataProvider converterDataProvider
+     * @param string $from
+     * @param string $to
+     */
+    public function testCanCorrectlyConvertFilenamesToLabels(string $from, string $to)
+    {
+        $converter = new FilenameToLabelConverter();
+        $this->assertSame($to, $converter->convert($from));
+    }
+}
